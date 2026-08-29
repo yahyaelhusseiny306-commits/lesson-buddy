d79de12e0f3a293d4b5c32fe828ce009ddb48d30
yahyaelhusseiny306-commits <yahyaelhusseiny306@gmail.com>
2026-08-30 00:42:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lesson-buddy-v26";
+const CACHE = "lesson-buddy-v28";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./libs.js?v=2"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
